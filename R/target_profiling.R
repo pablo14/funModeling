62141f13f14e2607a8e46a4220f6d2e10617f0dd
pablo14 <pcasas.biz@gmail.com>
2016-03-23 00:17:24 -0300

    several improvements to automate plot generation

--- R/target_profiling.R
+++ R/target_profiling.R
@@ -1,48 +1,66 @@
-
-
-
-run_graphics <- function(data, str_target, path_out)
+#' @title Get a summary for the given data frame.
+#' @description For each variable it returns: Quantity and percentage of zeros (q_zeros and p_zeros respectevly). Same metrics for NA values (q_NA and p_na). Last two columns indicates data type and quantity of unique values.
+#' This function print and return the results.
+#' @param data data frame
+#' @examples
+#' df_status(heart_disease)
+#' @return Metrics data frame
+#' @export
+gen_target_plots <- function(data, str_target, str_input, plot_type, path_out)
 {
 	if(missing(path_out)) path_out=NA
 
-	## Filtering only numeric variables
-	status=df_status(data)
-	colnam_num=status[!(status$type %in% "factor" | status$type %in% "factor"), 'variable']
+	if(missing(str_input))
+	{
+		## If missing=> Runs automatically for all numeric variables (valid only for numeric)
+		status=df_status(data)
+		str_input=status[!(status$type %in% "factor" | status$type %in% "factor"), 'variable']
+		## Excluding target variable (if it's detected as numeric)
+		str_input=str_input[str_input!=str_target]
 
-	## Excluding target variable (if it's detected as numeric)
-	colnam_num=colnam_num[colnam_num!=str_target]
+	}
 
-  for(i in 1:length(colnam_num))
+
+  for(i in 1:length(str_input))
   {
-    str_input=colnam_num[i]
-    print(str_input)
+    sprintf("Plotting '%s' ()", str_input[i], plot_type)
 
-    hist_targ=histdens_target(data, str_input, str_target)
-  	boxp_targ=boxplot_target(data, str_input, str_target)
+    ## Get the desiered plot
+		target_plot=get_target_plot(data, str_input[i], str_target, plot_type)
 
-  	## Save plot
+		plot(target_plot)
+
+		## Save plot
 	  if(!is.na(path_out))
 	  {
 	  	dir.create(path_out, showWarnings = F)
 
 	    if(dir.exists(path_out))
 	    {
-		     png(sprintf("%s/%s_hist.png", path_out, col_param), width= 12.25, height= 6.25, units="in",res=200, quality = 90)
-				 plot(hist_targ)
+		     jpeg(sprintf("%s/%s_%s.png", path_out, str_input[i], plot_type), width= 12.25, height= 6.25, units="in",res=200, quality = 90)
+				 plot(target_plot)
 	    dev.off()
-
-	    	png(sprintf("%s/%s_box.png", path_out, col_param), width= 12.25, height= 6.25, units="in",res=200, quality = 90)
-				plot(boxp_targ)
-	    	dev.off()
 	    } else {
 	      warning(sprintf("The directory '%s' doesn't exists.", path_out))
 	    }
 	  }
 
-
   }
 
 }
+
+get_target_plot <- function(data, str_input, str_target, plot_type)
+{
+	## Retrieve the desiered plot
+	if(plot_type=="histdens")
+     plot_target=histdens_target(data, str_input, str_target)
+
+  if(plot_type=="boxplot")
+    plot_target=boxplot_target(data, str_input, str_target)
+
+	return(plot_target)
+}
+
 
 #' @title Get a summary for the given data frame.
 #' @description For each variable it returns: Quantity and percentage of zeros (q_zeros and p_zeros respectevly). Same metrics for NA values (q_NA and p_na). Last two columns indicates data type and quantity of unique values.
@@ -56,10 +74,12 @@ histdens_target <- function(data, str_input, str_target)
 {
   cdf=ddply(data, str_target, .fun = function(d)
     c(
-      "var.mean" = round(mean(d[,str_input],na.rm=TRUE),2)
+      "var.mean" = round(mean(d[,str_input], na.rm=TRUE),2)
     ))
 
-  return(ggplot(data, aes_string(x=str_input, colour=str_target)) + geom_density() + geom_vline(data=cdf, aes_string(xintercept="var.mean",  colour=str_target), linetype="dashed", size=0.5))
+  plot_histdens=ggplot(data, aes_string(x=str_input, colour=str_target)) + geom_density() + geom_vline(data=cdf, aes_string(xintercept="var.mean",  colour=str_target), linetype="dashed", size=0.5)
+
+  return(plot_histdens)
 
 }
 
@@ -73,11 +93,10 @@ histdens_target <- function(data, str_input, str_target)
 #' @export
 boxplot_target <- function(data, str_input, str_target)
 {
+	plot_box=ggplot(data, aes_string(x=str_target, y=str_input, fill=str_target)) + geom_boxplot() +
+         guides(fill=FALSE)+stat_summary(fun.y=mean, geom="point", shape=5, size=4)
 
-  return(ggplot(data, aes_string(x=str_target, y=str_input, fill=str_target)) + geom_boxplot() +
-         guides(fill=FALSE)+stat_summary(fun.y=mean, geom="point", shape=5, size=4))
-
-
+	return(plot_box)
 }
 
 # get_cor_matrix <- function(data, target)
