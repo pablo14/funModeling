126646e9ff46d3f0836c8ab5858c637bde7a84cb
Oblap houses <pablo.casas@auth0.com>
2016-04-11 17:46:21 -0300

    added compare df function

--- R/numbers.R
+++ R/numbers.R
@@ -46,3 +46,51 @@ prep_outliers <- function(data, str_input, prob)
 
  return(data)
 }
+
+#' @title Compare two vectors of keys
+#' @description Obtain correlation table of all variables that belongs to data against target variable
+#' @param data data frame
+#' @param str_target string variable to predict
+#' @examples
+#' v1=c(1,2,4)
+#' v2=c(1,2,5,6)
+#' res=compare_df(key_x=v1, key_y=v2)
+#' # Print the keys that didn't match
+#' res
+#' # Accessing the keys not present in
+#' @return Correlation index for all data input variable
+#' @export
+compare_df <- function(key_x, key_y)
+{
+	# key_x=v1;key_y=v2
+  df_x=data.frame(key_x=key_x, flag_x=1)
+  df_y=data.frame(key_y=key_y, flag_y=1)
+
+  df_x$key_x=as.character(df_x$key_x)
+	df_y$key_y=as.character(df_y$key_y)
+
+  merge_all=merge(df_x, df_y, by.x='key_x', by.y='key_y', all=T)
+
+  names(merge_all)[1]="key"
+
+  merge_all_nona=merge_all[!is.na(merge_all$flag_x) & !is.na(merge_all$flag_y),]
+
+  not_in_x=merge_all[is.na(merge_all$flag_x),]
+  not_in_y=merge_all[is.na(merge_all$flag_y),]
+
+  print(sprintf("Coincident in both: %s", nrow(merge_all_nona)))
+  print(sprintf("Rows not present in X: %s", nrow(not_in_x)))
+  print(sprintf("Rows not present in Y: %s", nrow(not_in_y)))
+
+
+  list_diff=list()
+
+  list_diff[[1]]=list(
+    present_in_both=merge_all_nona$key,
+    rows_not_in_X=not_in_x$key,
+    rows_not_in_Y=not_in_y$key
+  	)
+
+  return(list_diff)
+}
+
