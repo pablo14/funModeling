d31092c89b3945ddb01e74ea04a61a532ad34b1f
pablo14 <pcasas.biz@gmail.com>
2016-06-11 21:32:54 -0300

    work on  prep_outlier

--- R/common_lib.R
+++ R/common_lib.R
@@ -30,29 +30,25 @@ check_target_existance <- function(data, str_target)
 	if(!(str_target %in% colnames(data))) stop(sprintf("Target variable '%s' does not exists in the data", str_target))
 }
 
-give_me_num_vars <-function(data, str_target, plot_type)
+give_me_num_vars <- function(data, str_target=NULL)
 {
-	## If missing => Runs automatically for all numeric variables (valid only for numeric)
+	##
 	status=df_status(data, print_results = F)
-	## select all columns that not are factor nor character
+
+	## Excluding not numeric variables
 	str_input=status[!(status$type %in% "factor" | status$type %in% "character"), 'variable']
+
 	## Excluding target variable (in the case that it's detected as numeric)
-	str_input=str_input[str_input!=str_target]
-
-	## Excluding variables with 2 unique values in boxplot
-	binary_variables=status[status$unique==2, 'variable']
-	str_input=str_input[!(str_input %in% binary_variables)]
-
-	if(length(binary_variables)>0)
-		sprintf('Binary variables removed: %s', paste(binary_variables,collapse = ', '))
+	#if(!missing(str_target))
+	#	str_input=str_input[str_input!=str_target]
 
 
-	## Excluding variables with a one unique value
-	one_value_variables=status[status$unique==1, 'variable']
-	str_input=str_input[!(str_input %in% one_value_variables)]
+	## Excluding variables with less than two unique value
+	ex_variables=status[status$unique<=2, 'variable']
+	str_input=str_input[!(str_input %in% ex_variables)]
 
-	if(length(one_value_variables)>0)
-		sprintf('Variables with one value removed: %s', paste(one_value_variables,collapse = ', '))
+	if(length(ex_variables)>0)
+		sprintf('Excluding variables with 1 or 2 unique values: %s', paste(ex_variables, collapse = ', '))
 
 
 	return(str_input)
--- R/numbers.R
+++ R/numbers.R
@@ -1,63 +1,52 @@
-#' @title Correlation index against target variable
-#' @description Obtain correlation table of all variables that belongs to data against target variable
-#' @param data data frame
-#' @param str_target string variable to predict
-#' @examples
-#' correlation_table(data=heart_disease, str_target="has_heart_disease")
-#' @return Correlation index for all data input variable
-#' @export
-correlation_table <- function(data, str_target)
-{
-	data[, str_target]=as.numeric(data[, str_target])
-
-	data=data[, c(give_me_num_vars(data, str_target), str_target)]
-
-  df_cor=as.data.frame(round(cor(data, use="complete.obs"	),2))
-  df_cor$Variable = rownames(df_cor)
-  df_cor=df_cor[, names(df_cor) %in% c(str_target, "Variable")]
-
-  row.names(df_cor) = NULL
-  df_cor=df_cor[, c(2,1)]
-
-  df_cor
-  return(df_cor)
-}
-
-
 #' @title Outliers Data Preparation
 #' @description
-#' Deal with outliers by removing or stopping them. Removes put an NA in the highest/lowers values at a certain percentile (top_percent/button_percent respectively).
-#' Removing is recommended when describing the general population metrics, parameter: type='remove'.
+#' Deal with outliers by setting an NA value or by 'stopping'. The first method puts an NA in the highest/lowest values at a certain percentile (parameters: 'top_percent'/'bottom_percent', respectively).
+#' Setting NA is recommended when describing the general population metrics, parameter: type='set_na'.
 #' Stopping is recommended when creating a predictive model without bias the result due to outliers, parameter: type='stop'.
 #' @param data data frame
 #' @param str_input string input variable (if empty, it runs for all numeric variable).
-#' @param top_percent value from 0 to 1, represents the highest X% of values to treat
-#' @param button_percent value from 0 to 1, represents the lowest X% of values to treat
-#' @param type can be 'stop' or 'remove', in the first case the original variable is stopped at the desiered percentile, 'remove'  sets NA to the same values.
+#' @param top_percent value from 0 to 1, represents the highest X percentage of values to treat
+#' @param bottom_percent value from 0 to 1, represents the lowest X percentage of values to treat
+#' @param type can be 'stop' or 'set_na', in the first case the original variable is stopped at the desiered percentile, 'set_na'  sets NA to the same values.
 #' @examples
-#' set.seed(10)
+#'set.seed(10)
 #'
-# Creating variable with outliers
-#' df=data.frame(var=rchisq(1000,df = 1))
-#' df=rbind(df, 1135, 2432) # forcing outliers
-#' summary(df$var) ## mean is 4.64, and max 2432
-
-##
-# Removing highest 1% of data. Type='remove' sets NA to highest value)
-#' res=prep_outliers(data = df, str_input = 'var', top_percent  = 0.01,  type='remove')
-#' summary(res) ## now the mean and the other metrics represent better the population
-
-# Removing highest 1% of data. Type='stop' sets the value of top_percent (7 in this case) to the highest values
-#' res=prep_outliers(data = df, str_input = 'var', top_percent  = 0.01,  type='stop')
-#' summary(res) ## metrics represents better the population, this escenario is good when creating a predictive model, since not every alghorimg handles well the outliers
-#' @return
-#' A vector if str_input contains only one variable, or the input data frame with the transformed variables if str_input has 2 or more
+#'# Creating variable with outliers
+#'df=data.frame(var1=rchisq(1000,df = 1), var2=rnorm(1000))
+#'df=rbind(df, 1135, 2432) # forcing outliers
+#'df$id=as.character(seq(1:1002))
+#'
+#'# for var1: mean is ~ 4.56, and max 2432
+#'summary(df)
+#'
+#'# checking the value for the top 1% of highest values (percentile 0.99), which is: 7.05
+#'quantile(df$var1, 0.99)
+#'
+#'#### EXAMPLE 1: Removing top 1% for a single variable
+#'# Setting type='set_na' sets NA to the highest value)
+#'var1_treated=prep_outliers(data = df,  str_input = 'var1', top_percent  = 0.01,  type='set_na')
+#'
+#'# now the mean (~ 0.94) is more adecuate, and note the 1st, median and 3rd quartiles remaing very similar to the original variable
+#'summary(var1_treated)
+#'
+#'#### EXAMPLE 2: if 'str_input' is missing, then it runs for all numeric variables.
+#'df_treated2=prep_outliers(data = df, top_percent  = 0.01, type='set_na')
+#'summary(df_treated2)
+#'
+#'#### EXAMPLE 3: Removing top 1% and bottom 1% for 'N' specific variables.
+#'vars_to_process=c('var1', 'var2')
+#'df_treated3=prep_outliers(data = df, str_input = vars_to_process, button_percent = 0.01, top_percent  = 0.01, type='set_na')
+#'summary(df_treated3)
+#'
+#' @return A vector if str_input contains only one variable, or the input data frame with the transformed variables if str_input has 2 or more
 #' @export
-prep_outliers <- function(data, str_input, top_percent, button_percent, type=c('stop', 'remove'))
+prep_outliers <- function(data, str_input, top_percent, bottom_percent, type=c('stop', 'set_na'))
 {
+	if(!(type %in% c('stop', 'set_na')))
+		stop("Parameter 'type' must be one 'stop' or 'set_na'")
 
-	if(!(type %in% c('stop', 'remove')))
-		stop("Parameter 'type' must be one 'stop' or 'remove'")
+	if(missing(str_input))
+		str_input=give_me_num_vars(data)
 
 	## Logic for top value
 	if(!missing(top_percent))
@@ -69,13 +58,13 @@ prep_outliers <- function(data, str_input, top_percent, button_percent, type=c('
 	  }
 	}
 
-	## Logic for button value
-	if(!missing(button_percent))
+	## Logic for bottom value
+	if(!missing(bottom_percent))
 	{
 	  for(i in 1:length(str_input))
 	  {
-	   	button_value=round(quantile(data[,str_input[i]], probs=button_percent, names=F, na.rm=T))
-	   	data[, str_input[i]][data[, str_input[i]]<button_value]=ifelse(type=='stop', button_value, NA)
+	   	bottom_value=round(quantile(data[,str_input[i]], probs=bottom_percent, names=F, na.rm=T))
+	   	data[, str_input[i]][data[, str_input[i]]<bottom_value]=ifelse(type=='stop', bottom_value, NA)
 	  }
 	}
 
@@ -135,3 +124,27 @@ compare_df <- function(key_x, key_y)
   return(res)
 }
 
+#' @title Correlation index against target variable
+#' @description Obtain correlation table of all variables that belongs to data against target variable
+#' @param data data frame
+#' @param str_target string variable to predict
+#' @examples
+#' correlation_table(data=heart_disease, str_target="has_heart_disease")
+#' @return Correlation index for all data input variable
+#' @export
+correlation_table <- function(data, str_target)
+{
+	data[, str_target]=as.numeric(data[, str_target])
+
+	data=data[, c(give_me_num_vars(data, str_target), str_target)]
+
+  df_cor=as.data.frame(round(cor(data, use="complete.obs"	),2))
+  df_cor$Variable = rownames(df_cor)
+  df_cor=df_cor[, names(df_cor) %in% c(str_target, "Variable")]
+
+  row.names(df_cor) = NULL
+  df_cor=df_cor[, c(2,1)]
+
+  df_cor
+  return(df_cor)
+}
