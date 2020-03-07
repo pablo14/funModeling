72eebd477f9a9840aac10017c0f0f8b379f41cff
pablo14 <pcasas.biz@gmail.com>
2020-03-07 17:06:30 -0300

    no message

--- R/attach.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(...)
-{
-   packageStartupMessage(sprintf("funModeling v.%s :)\nExamples and tutorials at livebook.datascienceheroes.com\n / Now in Spanish: librovivodecienciadedatos.ai", packageVersion("funModeling")))
-}
